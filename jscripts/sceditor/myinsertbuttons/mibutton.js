465eabb0be61d6fe6c8a576a663bd3aeb686150a
martec <mr.martchero@gmail.com>
2015-03-26 22:23:55 +0900

    solve mistake in js

--- jscripts/sceditor/myinsertbuttons/mibutton.js
+++ jscripts/sceditor/myinsertbuttons/mibutton.js
@@ -9,9 +9,9 @@ function mibimage(but_name) {
 function mibutton(but_name) {
 	$.sceditor.command.set(''+but_name+'', {
 		exec: function () {
-			this.insert('['+but_name+']', '['+but_name+']');
+			this.insert('['+but_name+']', '[/'+but_name+']');
 		},
-		txtExec: ['['+but_name+']', '['+but_name+']'],
+		txtExec: ['['+but_name+']', '[/'+but_name+']'],
 		tooltip: 'Insert a '+but_name+''
 	});
 }
