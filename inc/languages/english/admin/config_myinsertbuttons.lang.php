9c6bd406af83e5bc65caf5e3c58767b08c7b472d
martec <mr.martchero@gmail.com>
2015-03-26 22:00:33 +0900

    first release

---
+++ inc/languages/english/admin/config_myinsertbuttons.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$l['myinsertbuttons_plug_desc'] = 'Insert new button in Sceditor';
+$l['myinsertbuttons_sett_desc'] = 'Settings related to the My Insert Buttons.';
+$l['myinsertbuttons_rules_title'] = 'Add new button in Clickable MyCode Editor';
+$l['myinsertbuttons_rules_desc'] = 'Enter the button name. You must make sure the rule is valid and safe—no validation is performed. Separate buttons with ",".<br /><strong>Example:</strong> spoiler,test';
+
+?>
---
+++ jscripts/sceditor/myinsertbuttons/mibutton.js
@@ -0,0 +1,17 @@
+function mibimage(but_name) {
+	button = '<style type="text/css">'+
+		'.sceditor-button-'+but_name+' div  {'+
+			'background-image: url('+rootpath+'/jscripts/sceditor/myinsertbuttons/'+but_name+'.png) !important;'+
+		'}'+
+	'</style>';	
+	return button;
+}	
+function mibutton(but_name) {
+	$.sceditor.command.set(''+but_name+'', {
+		exec: function () {
+			this.insert('['+but_name+']', '['+but_name+']');
+		},
+		txtExec: ['['+but_name+']', '['+but_name+']'],
+		tooltip: 'Insert a '+but_name+''
+	});
+}
